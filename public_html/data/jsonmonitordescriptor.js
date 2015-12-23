88061eeb93bb11c1ac7c11e0c3732902671d52c3
Christophe Vimbert <c.vimbert@outlook.fr>
2015-12-23 22:21:12 +0100

    Working on angular monitor view

--- public_html/data/jsonmonitordescriptor.js
+++ public_html/data/jsonmonitordescriptor.js
@@ -1,13 +1,17 @@
 var monitorDesc = {
-    monitors: {
-        variables1: {
-            name: "Variables String"
-        },
-        variables2: {
-            name: "Variables Booléennes"
+    panelsset1: {
+        template: "",
+        panels: {
+            panel1: {
+                name: "Panneau des sprites",
+                type: "Sprite",
+                containerid: "centera1"
+            },
+            panel2: {
+                name: "Panneau des variables",
+                type: "Variable",
+                containerid: "centera2"
+            }
         }
-    },
-    layouts: {
-        
     }
 };
--- public_html/tentacle/js/modelmonitor/panelscontroller.js
+++ public_html/tentacle/js/modelmonitor/panelscontroller.js
@@ -1,4 +1,4 @@
-/* global angular, Tentacle */
+/* global angular, Tentacle, modelDescriptorV3, monitorDesc, _ */
 
 var mainApp = angular.module("monitoring-panel", ['ngRoute']);
 
@@ -10,10 +10,11 @@ mainApp.config(['$routeProvider',
                 when('/', {
                     template: tmpl,
                     controller: 'panelscontroller'
-                }).
+                });
+                        /*.
                 otherwise({
                     redirectTo: '/'
-                });
+                });*/
     }]);
 
 mainApp.controller("panelscontroller", function ($scope) {
@@ -21,29 +22,54 @@ mainApp.controller("panelscontroller", function ($scope) {
 });
 
 mainApp.controller("modelmonitorcontroller", function ($scope) {
-
+    
+    $scope.getModels = function () {
+        $scope.models = Tentacle.modelManager.getModelByType($scope.modeltype);
+    };
 });
 
 $(document).ready(function () {
+    var app = new Tentacle.MonitoringApp();
+});
 
+Tentacle.MonitoringApp = function () {
+    var modelManager = new Tentacle.ModelManager();
+    modelManager.init(modelDescriptorV3);
+    
+    modelManager.addModel("Sprite");
+    modelManager.addModel("Sprite");
+    modelManager.addModel("Variable");
+    
+    // voir si il existe une manière plus judicieuse de rendre dispo le modelManager
+    Tentacle.modelManager = modelManager;
+    
     $.get("includes/layout.html", function (data) {
         tmpl = $(data);
-        
-        var panel1 = new Tentacle.MonitorPanel("p1", "Panel 1", "centera1");
-        panel1.create();
-        var panel2 = new Tentacle.MonitorPanel("p2", "Panel 2", "centera2");
-        panel2.create();
+
+        var panelsSet = new Tentacle.MonitorPanelsSet("panelsset1");
 
         angular.bootstrap(document, ["monitoring-panel"]);
     });
+};
 
+Tentacle.MonitorPanelsSet = function (panelId) {
+    var panelDesc = monitorDesc[panelId];
 
-});
+    for (var uPanelId in panelDesc.panels) {
+        var uPanelDesc = panelDesc.panels[uPanelId];
 
-Tentacle.MonitorPanel = function (id, name, containerId) {
+        var panel = new Tentacle.MonitorPanel(uPanelId, uPanelDesc);
+        panel.create();
+    }
+};
+
+Tentacle.MonitorPanel = function (id, panelDesc) {
 
     this.create = function () {
-        var html = '<div id="' + id + '" ng-init="name=\'' + name + '\'" ng-controller="modelmonitorcontroller" ng-include="\'includes/basemonitor.html\'">Container1</div>';
-        $("#" + containerId, tmpl).append(html);
+        var tp = _.template('<div id="<%= id %>" ng-init="name=\'<%= name %>\'; modeltype=\'<%= modeltype %>\';getModels();" ng-controller="modelmonitorcontroller" ng-include="\'includes/basemonitor.html\'">Container1</div>');
+        var html = tp({id: id, name: panelDesc.name, modeltype: panelDesc.type});
+
+        $("#" + panelDesc.containerid, tmpl).append(html);
     };
+
 };
